08bdf4abeff495133ed2ed37f089534e7c69bc51
Tayo <adetayosunmonu@gmail.com>
2025-07-27 23:02:12 +0100

    adding interface and creating import

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,10 +1,5 @@
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
-
+import React from 'react';
+import { ButtonProps } from "@/interface";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
